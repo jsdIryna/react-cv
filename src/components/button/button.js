df9ac4ebe4baa038dc25fd0497921f4a1c59a8d0
Iryna Klyonova <iro4ka240404@gmail.com>
2023-10-07 01:35:02 +0300

    Created a Header component and added the main components to the app

---
+++ src/components/button/button.js
@@ -0,0 +1,14 @@
+// import '../../app.css';
+// function Button({ children = 'default text', color, customClass }) {
+//     return (
+//         <>
+//             {/* <button className={custonClass}> */}
+//             <button style={{ background: color }}>
+//                 {/* My Button component */}
+//                 {children}
+//             </button>
+//         </>
+//     );
+// }
+
+// export default Button;
---
+++ src/components/header/header.js
@@ -0,0 +1,32 @@
+import './header.css';
+import '../../app.css';
+import logo from '../../assets/burger.png';
+
+function Header() {
+    return (
+        <div className="header">
+            <div className="container">
+                <div className="header__row">
+                    <div className="header__logo">
+                        <div>ik</div>
+                    </div>
+                    <div className="burger">
+                        <img src={logo} alt="burger" />
+                    </div>
+                    <ul className="header__list">
+                        <li className="list__item__main"><a href="#">home</a></li>
+                        <li id="hover" className="list__item"><a href="#">works</a>
+                            <ul className="sub-menu">
+                                <li className="underline"><a href="calculator.html">Calculator</a></li>
+                                <li className="underline"><a href="">To do list</a></li>
+                            </ul>
+                        </li>
+                        <li className="list__item"><a href="contacts.html">contacts</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
