17b8d52c69fd1d7856cc4da25c2f762565bcfa48
Iryna Klyonova <iro4ka240404@gmail.com>
2024-07-24 18:13:19 +0300

    fixed tooltip errors and changed main photo

--- src/components/main-description/tooltip.jsx
+++ src/components/main-description/tooltip.jsx
@@ -1,8 +1,10 @@
 import 'components/main-description/tooltip.scss'
 
 const Tooltip = ({text}) =>
-    <div className='tooltip_container'> 
+    <div className='tooltip_container'>
+    <div className='tooltip_contant'> 
         <span className='tooltip_text'>{text}</span>
+    </div>
     </div>
   
 export default Tooltip;
